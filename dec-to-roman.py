7a5bead05b975077c8cb804440cf72b8ad96576a
Igor wojewnik <igor_wojewnik@sluz.ch>
2026-01-19 10:53:00 +0100

    Aufgabe 12

---
+++ dec-to-roman.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import sys
+
+value = sys.argv[1]
+
+
+if value == "1":
+    print("I")
+elif value == "2":
+    print("II")
+elif value == "3":
+    print("III")
+elif value == "4":
+    print("IV")
+elif value == "5":
+    print("V")
+elif value == "6":
+    print("VI")
+elif value == "7":
+    print("VII")
+elif value == "8":
+    print("VIII")
+elif value == "9":
+    print("IX")
+print("Die spinnen, die Römer")
